41ec02b356bc132f345ec03d449a708c6553dd3c
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2024-01-16 20:22:58 +0530

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,10 +1,10 @@
-import os
+timport os
 
 class Config:
-    API_ID = int( os.getenv("api_id","xxxx") )
-    API_HASH = os.getenv("api_hash","53ab7cxxxxxxxxc7bbebxxxx")
-    CHANNEL = int( os.getenv("channel_files_chat_id","-1xxxxxx0") )
-    CHANNEL_USERNAME = os.getenv("channel_username","testchannel")
-    TOKEN = os.getenv("token","xxxxx")
-    DOMAIN  = os.getenv("domain","https://testapp.herokuapp.com")
+    API_ID = int( os.getenv("api_id","15191874") )
+    API_HASH = os.getenv("api_hash","3037d39233c6fad9b80d83bb8a339a07")
+    CHANNEL = int( os.getenv("channel_files_chat_id","-1002029287584") )
+    CHANNEL_USERNAME = os.getenv("channel_username","irish_family")
+    TOKEN = os.getenv("token","6808668384:AAGKNPT24tLupcU_rKEys4JK-U3Yi2jLloc")
+    DOMAIN  = os.getenv("domain","https://ytdlbot-e25fc23a1ac0.herokuapp.com/")
  
